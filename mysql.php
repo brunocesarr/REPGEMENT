c1234ff3356df0fe8c96faa340fdffbf36ce02d6
Bruno César Silva <brunoc.15@hotmail.com>
2018-11-22 01:15:33 -0200

    Login Incompleto

---
+++ mysql.php
@@ -0,0 +1,29 @@
+<?php
+	class Conexao {
+
+    private $link;
+
+    function __construct() {
+        //  $this->link = mysqli_connect("localhost", "root", "", "TPBDI");
+        $this->link = new PDO("mysql:host=localhost;dbname=SistemaWEB", "root", "");
+    }
+
+    function conexao() {
+        if (!$this->link) {
+            echo "Error: Conexão interrompida com MySQL." . PHP_EOL;
+            return null;
+        } else {
+            //  echo "Sucesso!</br>";
+            return $this->link;
+        }
+    }
+
+    function busca($query) {
+        $consulta = $this->link->prepare($query);
+        $consulta->execute();
+        $linha = $consulta->fetchAll(PDO::FETCH_OBJ);
+        return $linha;
+    }
+
+	}
+?>
--- validar.php
+++ validar.php
@@ -1,4 +1,32 @@
 <?php
+	//	Inclui o arquivo de conexão com a base de dados.
+	include_once 'mysql.php';
 
+	// Recupera o login 
+	$email = (isset($_POST['email'])) ? $_POST['email'] : ''; 
+	// Recupera a senha, a criptografando em MD5 
+	$senha = (isset($_POST['senha'])) ? $_POST['senha'] : '';
+
+	//	Realiza a busca na base de dados
+	$con = new Conexao();
+	$query = "SELECT f_validar3('$email','$senha');";
+	$link = $con->conexao();
+	$sql = $link->prepare($query);
+	$sql->execute();
+	$linha = $sql->fetchAll();
+
+	echo "<pre>";
+
+	print_r($linha);
+
+	echo "</pre>";
+	//	Verifica o acesso ao usuário e redireciona a página correta
+	if($linha != null){
+		session_start();
+		header("Location:./home.html");
+	} else {	
+		echo"<script language='javascript' type='text/javascript'>alert('Login e/ou senha incorretos');window.location.href='index.html';</script>";
+        die();
+        header("Location:./index.html");
+	}
 ?>
-
